f19dc2cabb02139ee3d6f9ab21df9efaa628fe9d
Martha-Zhao <34574457+Martha-Zhao@users.noreply.github.com>
2019-07-02 17:39:29 +0800

    INIT functioon

--- TSL1401_init.c
+++ TSL1401_init.c
@@ -1,3 +1,8 @@
+/*
+* TSL1401 init file
+* rewrite in 0702
+*/
+
 /*-------------曝光延时-------------*/
 void Integration_Delay(uint16_t t)
 {
@@ -5,9 +10,9 @@ void Integration_Delay(uint16_t t)
 	for(x = 0;x < t;x++){
 		for(y = 0; y < 8000; y++);
 	}
-//    LPLD_LPTMR_DelayMs(t);
 }
 
+/*-------------simple delay-------------*/
 void Delay_ms(uint16_t time){
 	uint16_t x,y;                  //按外部晶振8MHZ算的延时
 	for(x = 0;x < time;x++){
@@ -15,6 +20,7 @@ void Delay_ms(uint16_t time){
 	}
 }
 
+/*-------------ADC0 and ADC1-------------*/
 float ADC1_SE_Get(void){        //ADC1采集数据
 	float val;
 	ADC1->SC1[0] &= ~ADC_SC1_ADCH_MASK; //清除ADCH
@@ -23,7 +29,9 @@ float ADC1_SE_Get(void){        //ADC1采集数据
 	val = (float)(ADC1->R[0]);              //读取数据
 	return val;
 }
-float ADC0_SE_Get(void){        //ADC1采集数据
+
+
+float ADC0_SE_Get(void){        //ADC0采集数据
 	float val;
 	ADC0->SC1[0] &= ~ADC_SC1_ADCH_MASK; //清除ADCH
 	ADC0->SC1[0] |= ADC_SC1_ADCH(8);    //开始转换，涉及通道ADC0_SE8
@@ -31,111 +39,62 @@ float ADC0_SE_Get(void){        //ADC1采集数据
 	val = (float)(ADC0->R[0]);              //读取数据
 	return val;
 }
+
 /*-------------单次采集-------------*/
-void TSL1401_GetLine_Oneshot_1(uint8_t *pixel)
+void TSL1401_GetLine_Oneshot(uint8_t *pixel， uint8_t ccd_select)
 {
-  uint8_t i;
-  //-----------单次采集曝光处理------------
-  TSL1401_SI_High;
-  asm("nop");
-  TSL1401_CLK_High;
-  asm("nop");//delay();
-  TSL1401_SI_Low;
-  //发送第1个CLK
-  asm("nop");
-  TSL1401_CLK_Low;
-  
-  //发送第2~129个CLK
-  for(i=1; i<129; i++)
-  {
+	if (ccd_select == 1){  //CCD1
+		TSL1401_SI_High		PTA->PSOR |= 1<<0   //SI 设置SI引脚高电平
+		TSL1401_SI_Low		PTA->PCOR |= 1<<0   //SI 设置低电平
+		TSL1401_CLK_High	PTB->PSOR |= 1<<10  //CLK设置高电平
+		TSL1401_CLK_Low	PTB->PCOR |= 1<<10 //CLK设置低电平
+	}
+	else if (ccd_select == 0){ //CCD0
+		TSL1401_SI_High   PTD->PSOR |= 1<<12
+		TSL1401_SI_Low    PTD->PCOR |= 1<<12
+		TSL1401_CLK_High  PTD->PSOR |= 1<<10
+		TSL1401_CLK_Low PTD->PCOR |= 1<<10
+	}
+
+	uint8_t i
+//-----------单次采集曝光处理------------
+	TSL14_SI_High;
 	asm("nop");
-    TSL1401_CLK_High;
-    asm("nop");
-    TSL1401_CLK_Low;
-  }
-  //曝光延时
-  Integration_Delay(20);//固定曝光时间 10ms
-  //采集上次曝光得到的图像
-  //开始SI
-  TSL1401_SI_High;
-  asm("nop");
-  TSL1401_CLK_High;
-  asm("nop");//delay();
-  TSL1401_SI_Low;
-  asm("nop");//delay();
-  //采集第1个点
-  pixel[0] = Normalized_U8(ADC1_SE_Get());
-  TSL1401_CLK_Low;
-  
-  //采集第2~128个点
-  for(i=1; i<128; i++)
-  {
+	TSL1401_CLK_High;
 	asm("nop");
-    TSL1401_CLK_High;
-    asm("nop");//delay();
-    pixel[i] = Normalized_U8(ADC1_SE_Get());
-    TSL1401_CLK_Low;
-  }
-  
-  //发送第129个clk
-  asm("nop");
-  TSL1401_CLK_High;
-  asm("nop");
-  TSL1401_CLK_Low;
-  asm("nop");
-  
-}
-
-void TSL1401_GetLine_Oneshot_0(uint8_t *pixel)
-{
-  uint8_t i;
-  //-----------单次采集曝光处理------------
-  CCD4_SI_High;
-  asm("nop");
-  CCD4_CLK_High;
-  asm("nop");//delay();
-  CCD4_SI_Low;
-  //发送第1个CLK
-  asm("nop");
-  CCD4_CLK_Low;
-
-  //发送第2~129个CLK
-  for(i=1; i<129; i++)
-  {
+	TSL1401_SI_Low;
+	
+	asm("nop");//发送第1个CLK
+	TSL1401_CLK_Low;
+	  
+	for(i=1; i<129; i++){//发送第2~129个CLK
+		asm("nop");
+		TSL1401_CLK_High;
+		asm("nop");
+		TSL1401_CLK_Low;
+	}
+	Integration_Delay(20);//固定曝光时间 10ms
+	  
+	TSL1401_SI_High;//开始SI
 	asm("nop");
-	CCD4_CLK_High;
-    asm("nop");
-    CCD4_CLK_Low;
-  }
-  //曝光延时
-  Integration_Delay(10);//固定曝光时间 10ms
-  //采集上次曝光得到的图像
-  //开始SI
-  CCD4_SI_High;
-  asm("nop");
-  CCD4_CLK_High;
-  asm("nop");//delay();
-  CCD4_SI_Low;
-  asm("nop");//delay();
-  //采集第1个点
-  pixel[0] = Normalized_U8(ADC0_SE_Get());
-  CCD4_CLK_Low;
-
-  //采集第2~128个点
-  for(i=1; i<128; i++)
-  {
+	TSL1401_CLK_High;
 	asm("nop");
-	CCD4_CLK_High;
-    asm("nop");//delay();
-    pixel[i] = Normalized_U8(ADC0_SE_Get());
-    CCD4_CLK_Low;
-  }
-
-  //发送第129个clk
-  asm("nop");
-  CCD4_CLK_High;
-  asm("nop");
-  CCD4_CLK_Low;
-  asm("nop");
-
+	TSL1401_SI_Low;
+	asm("nop");
+	pixel[0] = Normalized_U8(ADC1_SE_Get());//采集第1个点
+	TSL1401_CLK_Low;
+	
+	for(i=1; i<128; i++){//采集第2~128个点
+		asm("nop");
+		TSL1401_CLK_High;
+		asm("nop");//delay();
+		pixel[i] = Normalized_U8(ADC1_SE_Get());
+		TSL1401_CLK_Low;
+	}
+	
+	asm("nop");//发送第129个clk
+	TSL1401_CLK_High;
+	asm("nop");
+	TSL1401_CLK_Low;
+	asm("nop");
 }
